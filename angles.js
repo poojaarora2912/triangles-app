4afd10e7264a805be7f1afc3a7ace4b0524a9e28
poojaarora2912 <poojaarora2912@gmail.com>
2022-08-27 15:18:34 +0530

    add: null value check
    add: null value check

--- angles.js
+++ angles.js
@@ -3,12 +3,19 @@ const checkbutton = document.querySelector('#checktriangle');
 const output = document.querySelector('#output');
 
 function checktriangle(){
+    var a = Number(inputs[0].value);
+    var b = Number(inputs[1].value);
+    var c = Number(inputs[2].value);
+    if(a=='' || b=='' || c==''){
+        output.innerText="Enter valid values";
+    }else{
     const sum = sumofangles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
     if(sum===180){
         output.innerText="It is a triangle!";
     }else{
         output.innerText="It is not a triangle!";
     }
+ }
 }
 
 function sumofangles(angle1, angle2, angle3){
