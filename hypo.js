2dda99c791074dc66aac874c11d79d3005acf9b2
poojaarora2912 <poojaarora2912@gmail.com>
2022-08-27 15:09:02 +0530

    add: null check
    add: null check

--- hypo.js
+++ hypo.js
@@ -5,11 +5,19 @@ const output = document.querySelector("#output");
 calculate.addEventListener("click", result);
 
 function result(){
+var a = Number(inputs[0].value);
+var b = Number(inputs[1].value);
+if (a=='' || b==''){
+    output.innerText = "Enter valid values";
+}
+else{
 const sumofsquares = sum(Number(inputs[0].value), Number(inputs[1].value));
 const lengthofhypo = Math.sqrt(sumofsquares);
 output.innerText = "Length of Hypotenuse is:" + lengthofhypo;
 }
+}
 
 function sum(a,b){
     return a*a + b*b;
+    
 }
